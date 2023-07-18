fa7e0ff1d4a07b4b64224b8a2a9a5da30075a9b3
defaultzeroo <kaurov.ro@phystech.edu>
2023-07-18 13:57:42 +0300

    Created main.py
    As is

---
+++ first_tg_bot/main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = ''
+
+bot = Bot(TOKEN_API)
+dispatcher = Dispatcher(bot)
+
+
+@dispatcher.message_handler()
+async def msg(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher)
